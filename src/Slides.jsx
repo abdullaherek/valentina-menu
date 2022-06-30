b5bb33ff57c5779637b546519822328547565b0b
abdullaherek <abdullah.erek@ogrenci.bartin.edu.tr>
2022-06-30 21:07:43 +0300

    responsive image added

--- src/Slides.jsx
+++ src/Slides.jsx
@@ -16,17 +16,18 @@ const Slides = () => {
         <Slide>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${images[0]})` }}>
-                    <span>Slide 1</span>
                 </div>
             </div>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${images[1]})` }}>
-                    <span>Slide 2</span>
                 </div>
             </div>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${images[2]})` }}>
-                    <span>Slide 3</span>
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[3]})` }}>
                 </div>
             </div>
         </Slide>
