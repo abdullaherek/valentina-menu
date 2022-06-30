f4ab4be4f86c355eab68db71fba4d6e987d6b5b1
abdullaherek <abdullah.erek@ogrenci.bartin.edu.tr>
2022-06-30 20:58:38 +0300

    image source

--- src/Slides.jsx
+++ src/Slides.jsx
@@ -5,9 +5,11 @@ import './slides.css';
 
 const Slides = () => {
     const images = [
-        "https://pbs.twimg.com/media/FWcnj4ZWYAMn7A8?format=jpg&name=large",
-        "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-        "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+        "https://raw.githubusercontent.com/abdullaherek/valentina-menu/main/src/images/1.jpeg",
+        "https://raw.githubusercontent.com/abdullaherek/valentina-menu/main/src/images/2.jpeg",
+        "https://raw.githubusercontent.com/abdullaherek/valentina-menu/main/src/images/3.jpeg",
+        "https://raw.githubusercontent.com/abdullaherek/valentina-menu/main/src/images/4.jpeg",
+
     ];
 
     return (
